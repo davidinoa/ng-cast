fe4aa4536b4ea6adfc5b67cbd3f786d2197449ce
David Inoa <davidinoa@gmail.com>
2017-12-15 22:31:49 -0500

    Refactor the videoList component to dynamically render one videoListEntry component for each video

--- src/components/app.js
+++ src/components/app.js
@@ -8,24 +8,5 @@ angular.module('video-player')
       this.videos = window.exampleVideoData;
     },
 
-    // templateUrl: 'src/templates/app.html'
-
-    template: `
-      <div id="app container">
-        <nav class="navbar">
-          <div class="col-md-6 col-md-offset-3">
-            <search><h5><em>search</em> component goes here</h5></search>
-          </div>
-        </nav>
-        <div class="row">
-          <div class="col-md-7">
-            <video-player><h5><em>videoPlayer</em> component goes here</h5></video-player>
-          </div>
-          <div class="col-md-5">
-            <video-list videos="$ctrl.videos"></video-list>
-            <pre>{{ $ctrl.videos }}</pre>
-          </div>
-        <div>
-      </div>
-    `
+    templateUrl: 'src/templates/app.html'
   });
