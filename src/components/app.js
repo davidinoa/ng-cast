b24a3dbc5a61ed362bb9ea0b10a3a2fcab5e53d9
David Inoa <davidinoa@gmail.com>
2017-12-15 22:13:56 -0500

    Pass the data from exampleVideoData into the videoList component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,31 @@
 angular.module('video-player')
-
   .component('app', {
-    templateUrl: 'src/templates/app.html'
+    // bindings: {
+    //   videos: '<'
+    // },
+
+    controller: function () {
+      this.videos = window.exampleVideoData;
+    },
+
+    // templateUrl: 'src/templates/app.html'
+
+    template: `
+      <div id="app container">
+        <nav class="navbar">
+          <div class="col-md-6 col-md-offset-3">
+            <search><h5><em>search</em> component goes here</h5></search>
+          </div>
+        </nav>
+        <div class="row">
+          <div class="col-md-7">
+            <video-player><h5><em>videoPlayer</em> component goes here</h5></video-player>
+          </div>
+          <div class="col-md-5">
+            <video-list videos="$ctrl.videos"></video-list>
+            <pre>{{ $ctrl.videos }}</pre>
+          </div>
+        <div>
+      </div>
+    `
   });
